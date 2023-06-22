73672afa6b62b401f90ed55aba9422d6ff8a3b1b
Eslam Odeh <eslamodeh@outlook.com>
2023-06-22 11:45:24 +0100

    Install rebenv

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') > $HOME/.zprofile;
+eval "$(/opt/homebrew/bin/brew shellenv)";
 
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
-defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write -g InitialKeyRepeat -int 10; # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1; # normal minimum is 2 (30 ms)
 
-brew install vim neovim tmux fzf;
+brew install vim neovim tmux fzf tig rbenv;
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags;
 
 $(brew --prefix)/opt/fzf/install;
 
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
+rm ~/.config/nvim;
 ln -sF $(pwd)/nvim ~/.config/nvim;
 
-cp .zshrc .editorconfig .tmux.conf .iterm2_profile.json ~/
+cp .zshrc .editorconfig .tmux.conf .iterm2_profile.json ~/;
