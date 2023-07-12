a5de10a1d3dddee4fac7beeb72bc09e973e80472
Eslam Odeh <eslamodeh@outlook.com>
2023-07-12 14:26:07 +0100

    Add autoenv

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+
+# Autoload Brew
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') > $HOME/.zprofile;
+# Autoload autoenv
+printf '%s\n' "source $(brew --prefix autoenv)/activate.sh" >> "${ZDOTDIR:-$HOME}/.zprofile"
+
 eval "$(/opt/homebrew/bin/brew shellenv)";
 
 defaults write -g InitialKeyRepeat -int 10; # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1; # normal minimum is 2 (30 ms)
 
-brew install cmake vim neovim tmux fzf tig rbenv pyenv ag prettier;
+brew install cmake vim neovim tmux fzf tig rbenv pyenv ag prettier autoenv;
 pip3 install pynvim black pylint;
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags;
 
