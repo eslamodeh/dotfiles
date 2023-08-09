7ce702c98defc8b38ceaf73ba2c66aa619565fe5
Eslam Odeh <eslamodeh@outlook.com>
2023-08-09 17:00:32 +0100

    Move autoenv to the end

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@ echo "Installing Hombrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 # Autoload Brew
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') > $HOME/.zprofile;
-# Autoload autoenv
-printf '%s\n' "source $(brew --prefix autoenv)/activate.sh" >> "${ZDOTDIR:-$HOME}/.zprofile"
 eval "$(/opt/homebrew/bin/brew shellenv)";
 
 echo "Updating keyword speed"
@@ -17,12 +15,11 @@ defaults write -g InitialKeyRepeat -int 10; # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1; # normal minimum is 2 (30 ms)
 
 echo "Installing default packages"
-brew install gh cmake vim neovim tmux fzf tig ag prettier autoenv;
+brew install gh cmake vim neovim tmux fzf tig ag prettier;
 pip3 install pynvim; # for nvim
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags;
 brew install rbenv pyenv nvm;
 $(brew --prefix)/opt/fzf/install;
-
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
@@ -33,3 +30,8 @@ ln -sF $(pwd)/nvim ~/.config/nvim;
 
 echo "Copying configuration file"
 cp .zshrc .editorconfig .tmux.conf .iterm2_profile.json ~/;
+
+
+echo "Installing autoenv"
+brew install autoenv;
+printf '%s\n' "source $(brew --prefix autoenv)/activate.sh" >> "${ZDOTDIR:-$HOME}/.zprofile"
