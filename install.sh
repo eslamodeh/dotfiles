c3a244dba18486362286c499875038ce03276f63
Eslam Odeh <eslam.odeh@MacBook-Pro-4.local>
2023-08-09 16:51:26 +0100

    Improve installation script

--- install.sh
+++ install.sh
@@ -1,29 +1,35 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+set -e
 
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+echo "Installing Hombrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
 # Autoload Brew
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') > $HOME/.zprofile;
 # Autoload autoenv
 printf '%s\n' "source $(brew --prefix autoenv)/activate.sh" >> "${ZDOTDIR:-$HOME}/.zprofile"
-
 eval "$(/opt/homebrew/bin/brew shellenv)";
 
+echo "Updating keyword speed"
 defaults write -g InitialKeyRepeat -int 10; # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1; # normal minimum is 2 (30 ms)
 
-brew install cmake vim neovim tmux fzf tig ag prettier autoenv;
+echo "Installing default packages"
+brew install gh cmake vim neovim tmux fzf tig ag prettier autoenv;
+pip3 install pynvim; # for nvim
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags;
-brew install rbenv;
-brew install pyenv ruff;
-
-pip3 install pynvim black pylint;
-
+brew install rbenv pyenv nvm;
 $(brew --prefix)/opt/fzf/install;
 
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
+echo "Linking nvim"
 rm ~/.config/nvim;
 ln -sF $(pwd)/nvim ~/.config/nvim;
 
+echo "Copying configuration file"
 cp .zshrc .editorconfig .tmux.conf .iterm2_profile.json ~/;
